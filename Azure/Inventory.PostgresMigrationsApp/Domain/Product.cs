0a31cc339bac9ffc8b61767cb01c7d78544be4e5
strivitech <strivitech@gmail.com>
2024-04-22 21:29:55 +0300

    Add quantity to project db migration

--- Azure/Inventory.PostgresMigrationsApp/Domain/Product.cs
+++ Azure/Inventory.PostgresMigrationsApp/Domain/Product.cs
@@ -6,4 +6,5 @@ public class Product
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public decimal Price { get; set; }
+    public int Quantity { get; set; }
 }
