3aca01e061cd904c123fb34fb45cf7d92a6eaca5
strivitech <strivitech@gmail.com>
2024-04-30 01:09:27 +0300

    Add Database Constants

---
+++ Azure/Ordering.Functions/Common/DatabaseConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Functions.Common;
+
+public static class DatabaseConstants
+{
+    public const string DatabaseName = "OrdersDatabase";
+    public const string ContainerName = "Orders";
+}
