ee94378f0693309cd7d70b6a3a5b090c11829011
strivitech <strivitech@gmail.com>
2024-04-30 01:09:16 +0300

    Add QueryParameter Authorization Code Handler

---
+++ Azure/Ordering.Functions/Common/AddQueryParameterAuthorizationCodeHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+namespace Ordering.Functions.Common;
+
+public class AddQueryParameterAuthorizationCodeHandler(string code) : DelegatingHandler
+{
+    private readonly string _code = code;
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var uriBuilder = new UriBuilder(request.RequestUri!);
+        var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+        query["code"] = _code;
+        uriBuilder.Query = query.ToString();
+        request.RequestUri = uriBuilder.Uri;
+        return base.SendAsync(request, cancellationToken);
+    }
+}
